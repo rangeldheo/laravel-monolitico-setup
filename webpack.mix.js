d58c02edd64aaea572e496a370d439f72e658c20
rangeldheo <rangeldheo@gmail.com>
2020-10-13 12:05:44 -0300

    Instala o sistema de login e cadastro básico do Laravel + bootstrap

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,4 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .copy('vendor/almasaeed2010/adminlte/plugins', 'public/adminlte/plugins')
-    .copy('vendor/almasaeed2010/adminlte/dist', 'public/adminlte/dist')
     .sass('resources/sass/app.scss', 'public/css');
